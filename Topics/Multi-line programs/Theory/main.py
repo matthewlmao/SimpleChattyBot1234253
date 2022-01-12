35cc30ea75873857664eed03035199a32259e330
xiao <97486178+xiaoltd@users.noreply.github.com>
2022-01-12 09:20:55 +0800

    Initial commit

---
+++ Topics/Multi-line programs/Theory/main.py
@@ -0,0 +1,4 @@
+#  You can experiment here, it won’t be checked
+print("first")
+print("second")
+print("third")
---
+++ Topics/Program with numbers/Good rest on vacation/main.py
@@ -0,0 +1,6 @@
+days = int(input())
+food = int(input())
+flight = int(input())
+night = int(input())
+
+print(days * food + 2 * flight + (days * night - night))
